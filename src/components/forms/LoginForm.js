f4ec0eeaa3048ae63858bd8aeb18edc4db831cb6
Sandeep G. Khode <sgkhode@gmail.com>
2018-03-01 18:10:49 +0530

    Make async req to server and render server validation errors

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import InlineError from '../messages/InlineError';
 
@@ -19,7 +19,9 @@ class LoginForm extends React.Component {
         this.setState({ errors });
 
         if (Object.keys(errors).length === 0) { 
-            this.props.submit(this.state.data);
+            this.setState({ loading: true });
+            this.props.submit(this.state.data)
+                .catch(err => this.setState({ errors: err.response.data.errors, loading:false }));
         }
     }
 
@@ -33,14 +35,18 @@ class LoginForm extends React.Component {
     validate = (data) => { 
         const errors = {};
         if (!Validator.isEmail(data.email)) errors.email = "Invalid Email";
-        if (!data.password) errors.password = "Can't be black";
+        if (!data.password) errors.password = "Can't be blank";
         return errors;
     }
 
     render() { 
-        const { data, errors } = this.state;
-        return (    
-            <Form onSubmit={this.onSubmit}>
+        const { data, errors, loading } = this.state;
+        return (
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                {errors.global && <Message negative>
+                    <Message.Header>Sonething went wrong</Message.Header>
+                    <p>{errors.global}</p>
+            </Message> }    
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input
