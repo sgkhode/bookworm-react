8b812999c239df6c2a4e5ef5621d9367b48b3151
Sandeep G. Khode <sgkhode@gmail.com>
2018-03-05 10:30:51 +0530

    User signs up

---
+++ src/actions/users.js
@@ -0,0 +1,6 @@
+import { userLoggedIn } from "./auth";
+import api from '../api';
+
+export const signup = (data) => (dispatch) =>
+    api.user.signup(data)
+    .then(user => dispatch(userLoggedIn(user)));
